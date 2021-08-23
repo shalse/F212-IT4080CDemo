76d0079eaf240b1c01fcd0cb9abca92c36d77f55
Shane Halse <shanehalse@gmail.com>
2021-08-23 14:55:29 -0400

    Updated Hi script

--- Assets/Hi.cs
+++ Assets/Hi.cs
@@ -7,7 +7,7 @@ public class Hi : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //start comment
     }
 
     // Update is called once per frame
